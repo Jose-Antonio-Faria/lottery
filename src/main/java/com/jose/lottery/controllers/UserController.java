44faad0be11b62e18e1dbe3d881b9bb71a118a9c
JoseFaria <j.anton.faria@gmail.com>
2023-04-08 18:40:21 +0100

    Add UserController

---
+++ src/main/java/com/jose/lottery/controllers/UserController.java
@@ -0,0 +1,37 @@
+package com.jose.lottery.controllers;
+
+import com.jose.lottery.dtos.UserDto;
+import com.jose.lottery.models.UserModel;
+import com.jose.lottery.services.UserService;
+import jakarta.validation.Valid;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author jose
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> saveuser(@RequestBody @Valid UserDto userDto){
+        var userModel = new UserModel();
+        BeanUtils.copyProperties(userDto, userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(userModel));
+    }
+}
