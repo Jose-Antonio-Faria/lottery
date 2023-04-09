25807a8fa9790ed66d56be322b49bce591656216
JoseFaria <j.anton.faria@gmail.com>
2023-04-09 17:22:41 +0100

    Add UserRepository Test

---
+++ src/test/java/com/jose/lottery/repositories/UserRepositoryTest.java
@@ -0,0 +1,26 @@
+package com.jose.lottery.repositories;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+/**
+ *
+ * @author jose
+ */
+@DataJpaTest
+class UserRepositoryTest {
+
+  @Autowired private UserRepository userRepository;
+
+  @Test
+  void injectedComponentsAreNotNull(){
+    assertThat(userRepository).isNotNull();
+  }
+}
