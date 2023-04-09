deb34d2a5f0f6b2e860ba67b0f1e79e80cec0f6e
JoseFaria <j.anton.faria@gmail.com>
2023-04-09 14:12:58 +0100

    Use getTodayDate to set LotteryEvent date

--- src/main/java/com/jose/lottery/event/manager/LotteryEventManager.java
+++ src/main/java/com/jose/lottery/event/manager/LotteryEventManager.java
@@ -1,10 +1,13 @@
 package com.jose.lottery.event.manager;
 
+import com.jose.lottery.configs.DateConfig;
 import com.jose.lottery.models.LotteryEventModel;
 import com.jose.lottery.services.LotteryEventService;
+import com.jose.lottery.utils.DateUtils;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -24,16 +27,16 @@ public class LotteryEventManager {
 
 //    @Scheduled(fixedRate = 5000)
     public void execute() {
-        if(!lotteryEventService.existsByDate(LocalDateTime.now(ZoneId.of("UTC")))){
-            createLotteryEvent();     
+        if (!lotteryEventService.existsByDate(DateUtils.getTodayDate())) {
+            createLotteryEvent();
         }
     }
-    
-    private void createLotteryEvent(){
+
+    private void createLotteryEvent() {
         LotteryEventModel lotteryEventModel = new LotteryEventModel();
-        lotteryEventModel.setDate(LocalDateTime.now(ZoneId.of("UTC")));
+        lotteryEventModel.setDate(DateUtils.getTodayDate());
         lotteryEventModel.setStatus(LotteryEventModel.Status.OPEN);
         lotteryEventService.save(lotteryEventModel);
     }
-    
+
 }
