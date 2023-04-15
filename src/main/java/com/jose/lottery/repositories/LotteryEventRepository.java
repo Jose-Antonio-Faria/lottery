ce1603c9883ab2f3405bf376e941b20a3da259f6
JoseFaria <j.anton.faria@gmail.com>
2023-04-15 16:10:04 +0100

    Minor Changes

--- src/main/java/com/jose/lottery/repositories/LotteryEventRepository.java
+++ src/main/java/com/jose/lottery/repositories/LotteryEventRepository.java
@@ -1,10 +1,13 @@
 package com.jose.lottery.repositories;
 
+import com.jose.lottery.models.BallotModel;
 import com.jose.lottery.models.LotteryEventModel;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -16,4 +19,5 @@ public interface LotteryEventRepository extends JpaRepository<LotteryEventModel,
     
     public boolean existsByDate(LocalDateTime date);
     public Optional<LotteryEventModel> findByDate(LocalDateTime date);
+    
 }
